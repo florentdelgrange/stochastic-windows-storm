58e1e248fa9476b8b48fd6920a0c66c86386bc94
radioGiorgio <florent.delgrange@gmail.com>
2018-11-26 16:10:02 +0100

    Constructor example

---
+++ stochastic-windows/FooBar.h
@@ -0,0 +1,18 @@
+//
+// Created by Florent Delgrange on 30/10/2018.
+//
+
+#ifndef STORM_STOCHASTICWINDOWS_H
+#define STORM_STOCHASTICWINDOWS_H
+
+namespace sw {
+    class FooBarObject {
+    public:
+        FooBarObject();
+
+        int foo();
+    };
+}
+
+
+#endif //STORM_STOCHASTICWINDOWS_H
