75311b6bd5d93ebe518742a25ad40eec989ff406
radioGiorgio <florent.delgrange@gmail.com>
2018-11-28 14:18:21 +0100

    Unfolding debugged + getNewStatesMeaning function

--- stochastic-windows/ECsUnfolding.cpp
+++ stochastic-windows/ECsUnfolding.cpp
@@ -2,54 +2,60 @@
 // Created by Florent Delgrange on 2018-11-23.
 //
 
-#include <storm/storage/SparseMatrix.h>
-#include <storm/models/sparse/Model.h>
-#include <iostream>
-#include <storm/models/sparse/Mdp.h>
-#include <storm/storage/MaximalEndComponentDecomposition.h>
-#include "stochastic-windows/BndGoodWindowMP.h"
+#include <stochastic-windows/BndGoodWindowMP.h>
+#include "BndGoodWindowMP.h"
 
 
 template<typename ValueType>
 sw::BndGoodWindowMP::ECsUnfolding<ValueType>::ECsUnfolding(storm::models::sparse::Mdp<ValueType, storm::models::sparse::StandardRewardModel<ValueType>> const& mdp,
                                                            std::string const& rewardModelName,
                                                            uint_fast64_t const& l_max) {
-    storm::storage::SparseMatrix<ValueType> originalMatrix = mdp.getTransitionMatrix();
-    assert(mdp.asRewardModel(rewardModelName));
+    const storm::storage::SparseMatrix<ValueType> &originalMatrix = mdp.getTransitionMatrix();
+    assert(mdp.hasRewardModel(rewardModelName));
     storm::models::sparse::StandardRewardModel<ValueType> rewardModel = mdp.getRewardModel(rewardModelName);
     assert(rewardModel.hasStateActionRewards());
     std::vector<ValueType> stateActionRewardsVector = rewardModel.getStateActionRewardVector();
-    storm::storage::MaximalEndComponentDecomposition<ValueType> mecDecomposition(*mdp);
+    storm::storage::MaximalEndComponentDecomposition<ValueType> mecDecomposition(mdp);
 
+    this->l_max = l_max;
+    // Initialization of the matrices representing the unfolding of the ECS; note that the index 0 is a special one
+    // (and is thus reserved) representing the non-ECs states
     matrices = std::vector<storm::storage::SparseMatrix<ValueType>>(mecDecomposition.size() + 1);
-    // vector containing all data about the unfolding
+    // Initialization of the vector containing mec indices for each states
+    mecIndices = std::vector<uint_fast64_t>(mdp.getNumberOfStates());
+    // vector containing the data about states of the unfolding
     windowVector = std::vector<std::vector<std::unordered_map<ValueType, uint_fast64_t>>>(mdp.getNumberOfStates());
     storm::storage::MaximalEndComponent mec;
     // k = 0 is a special value meaning that the state does not belong to any MEC
     newRowGroupEntries.emplace_back();
-    for (uint_fast64_t k = 1; k < mecDecomposition.size() + 1; ++k) {
+    for (uint_fast64_t k = 1; k <= mecDecomposition.size(); ++ k) {
         mec = mecDecomposition[k - 1];
         // initialize the new row group entries for the kth mec
         newRowGroupEntries.emplace_back();
-        storm::storage::SparseMatrixBuilder<ValueType> matrixBuilder;
+        storm::storage::SparseMatrixBuilder<ValueType> matrixBuilder(0, 0, 0, true, true);
         for (auto state : mec.getStateSet()) {
             mecIndices[state] = k;
             windowVector[state] = std::vector<std::unordered_map<ValueType, uint_fast64_t>>(l_max + 1);
         }
         // Unfold the kth MEC
         for (auto state: mec.getStateSet()) {
-            unfoldFrom(state, 0., 0, l_max, originalMatrix, stateActionRewardsVector, mec);
+            unfoldFrom(state, 0., 0, originalMatrix, stateActionRewardsVector, mec);
         }
         // Build the new matrix w.r.t. the new row group entries computed during the unfolding
         uint_fast64_t newRow = 0;
         uint_fast64_t column;
         ValueType p;
-        for (auto entries : newRowGroupEntries[k]) {
+        // iterate on states (new row groups) of the unfolding of the kth MEC
+        for (const auto &newRowGroup : newRowGroupEntries[k]) {
             matrixBuilder.newRowGroup(newRow);
-            for (auto entry : entries) {
-                std::tie(column, p) = entry;
-                matrixBuilder.addNextValue(newRow, column, p);
-                ++newRow;
+            // iterate on rows, i.e., enabled actions of the new state considered at the current iteration
+            for (const auto &row : newRowGroup) {
+                // iterate on each outgoing transition
+                for (auto entry : row) {
+                    std::tie(column, p) = entry;
+                    matrixBuilder.addNextValue(newRow, column, p);
+                }
+                ++ newRow;
             }
         }
         matrices[k] = matrixBuilder.build();
@@ -62,9 +68,9 @@ uint_fast64_t sw::BndGoodWindowMP::ECsUnfolding<ValueType>::getMecIndex(uint_fas
 }
 
 template <typename ValueType>
-std::pair<uint_fast64_t, uint_fast64_t> sw::BndGoodWindowMP::ECsUnfolding<ValueType>::getNewIndex(uint_fast64_t state,
-                                                                                                  ValueType currentSumOfWeights,
-                                                                                                  uint_fast64_t currentWindowLength) {
+const std::pair<uint_fast64_t, uint_fast64_t> sw::BndGoodWindowMP::ECsUnfolding<ValueType>::getNewIndex(uint_fast64_t state,
+                                                                                                        ValueType currentSumOfWeights,
+                                                                                                        uint_fast64_t currentWindowLength) {
     uint_fast64_t k = getMecIndex(state);
     if ( !k ) return std::make_pair(0, 0);
     auto keyValue = windowVector[state][currentWindowLength].find(currentSumOfWeights);
@@ -77,9 +83,9 @@ std::pair<uint_fast64_t, uint_fast64_t> sw::BndGoodWindowMP::ECsUnfolding<ValueT
 }
 
 template <typename ValueType>
-uint_fast64_t sw::BndGoodWindowMP::ECsUnfolding<ValueType>::unfoldFrom(u_int64_t const& state, ValueType const& currentSumOfWeights,
+uint_fast64_t sw::BndGoodWindowMP::ECsUnfolding<ValueType>::unfoldFrom(uint_fast64_t const& state,
+                                                                       ValueType const& currentSumOfWeights,
                                                                        uint_fast64_t const& l,
-                                                                       uint_fast64_t const& l_max,
                                                                        storm::storage::SparseMatrix<ValueType> const &originalMatrix,
                                                                        std::vector<ValueType> const& stateActionRewardsVector,
                                                                        storm::storage::MaximalEndComponent const &currentMec){
@@ -88,9 +94,9 @@ uint_fast64_t sw::BndGoodWindowMP::ECsUnfolding<ValueType>::unfoldFrom(u_int64_t
     assert(newRowGroupEntries.size() > k);
     // Initialization of the new row group entries for the MEC k
     if (newRowGroupEntries[k].empty()) {
-        // the index 0 is the index of the sink state corresponding to state (s, w, l) where l > l_max and w < 0
+        // the index 0 is the index of the sink state corresponding to states (s, w, l) where l > l_max and w < 0
         newRowGroupEntries[k].emplace_back();
-        newRowGroupEntries[k][0].push_back(std::make_pair(0, 1.));
+        newRowGroupEntries[k][0].push_back({std::make_pair(0, 1.)});
     }
     // i is the row group index of (state, currentSomeOfWeights, l) in the new matrix
     uint_fast64_t i;
@@ -101,38 +107,44 @@ uint_fast64_t sw::BndGoodWindowMP::ECsUnfolding<ValueType>::unfoldFrom(u_int64_t
         newRowGroupEntries[k].emplace_back();
         windowVector[state][l][currentSumOfWeights] = i;
         // assert( windowVector[state][l].find( currentSumOfWeights )->second == i );
-        // if i was not in the map of weights, unfold the EC from the ith state s_i
+        // as i was not in the map of weights, unfold the EC from the ith state s_i
         ValueType updatedSumOfWeights;
         for (auto action : currentMec.getChoicesForState(state)) {
             updatedSumOfWeights = currentSumOfWeights + stateActionRewardsVector[action];
             uint_fast64_t l_new = l + 1;
+            // add the current action to the new row group entries
+            newRowGroupEntries[k][i].emplace_back();
+            uint_fast64_t newAction = newRowGroupEntries[k][i].size() - 1;
             // if reward(s_i, action) is >= 0 and l_new <= l_max, the window can be closed
             if (updatedSumOfWeights >= 0 and l_new <= l_max) {
                 // the indices in the enumeration of enabled action for each state in the mec correspond to the
                 // indices of rows in the original matrix
-                for (auto entry : originalMatrix.getRow(action)) {
+                for (const auto &entry : originalMatrix.getRow(action)) {
                     uint_fast64_t successorState = entry.getColumn();
                     ValueType p = entry.getValue();
                     // j is the index of successorState
-                    uint_fast64_t j = unfoldFrom(successorState, 0., 0, l_max, originalMatrix, stateActionRewardsVector,
+                    uint_fast64_t j = unfoldFrom(successorState, 0., 0, originalMatrix, stateActionRewardsVector,
                                                  currentMec);
-                    newRowGroupEntries[k][i].push_back(std::make_pair(j, p));
+                    newRowGroupEntries[k][i][newAction].push_back(std::make_pair(j, p));
                 }
             }
             else if (l_new <= l_max) {
-                for (auto entry : originalMatrix.getRow(action)) {
+                for (const auto &entry : originalMatrix.getRow(action)) {
                     uint_fast64_t successorState = entry.getColumn();
                     ValueType p = entry.getValue();
                     // j is the index of successorState
-                    uint_fast64_t j = unfoldFrom(successorState, updatedSumOfWeights, l_new, l_max, originalMatrix,
+                    uint_fast64_t j = unfoldFrom(successorState, updatedSumOfWeights, l_new, originalMatrix,
                                                  stateActionRewardsVector, currentMec);
-                    newRowGroupEntries[k][i].push_back(std::make_pair(j, p));
+                    newRowGroupEntries[k][i][newAction].push_back(std::make_pair(j, p));
                 }
             }
             else {
                 // if s_i = (s, w, l_max) with w < 0, then the bounded window objective happens with
                 // probability zero. Then, s_i transitions to the sink state.
-                newRowGroupEntries[k][i].push_back(std::make_pair(0, 1.));
+                newRowGroupEntries[k][i][newAction] = {std::make_pair(0, 1.)};
+                // Since the bound l_max is exceeded, we can ignore other useless actions necessarily leading to the
+                // sink state.
+                break;
             }
         }
     }
@@ -140,7 +152,41 @@ uint_fast64_t sw::BndGoodWindowMP::ECsUnfolding<ValueType>::unfoldFrom(u_int64_t
 }
 
 template <typename ValueType>
-storm::storage::SparseMatrix<ValueType> sw::BndGoodWindowMP::ECsUnfolding<ValueType>::getUnfoldedMatrix(uint_fast64_t mec) {
-    return matrices[mec - 1];
+const storm::storage::SparseMatrix<ValueType> sw::BndGoodWindowMP::ECsUnfolding<ValueType>::getUnfoldedMatrix(uint_fast64_t mec) {
+    return matrices[mec];
 }
 
+template<typename ValueType>
+uint_fast64_t sw::BndGoodWindowMP::ECsUnfolding<ValueType>::getNumberOfUnfoldedECs() {
+    return matrices.size() - 1;
+}
+
+template<typename ValueType>
+uint_fast64_t sw::BndGoodWindowMP::ECsUnfolding<ValueType>::getMaximumWindowsLength() {
+    return l_max;
+}
+
+template<typename ValueType>
+const std::vector<sw::BndGoodWindowMP::StateWeightWindowLength<ValueType>>
+sw::BndGoodWindowMP::ECsUnfolding<ValueType>::getNewStatesMeaning(uint_fast64_t k) {
+
+    std::vector<sw::BndGoodWindowMP::StateWeightWindowLength<ValueType>> unfoldedStates(newRowGroupEntries[k].size());
+
+    for (uint_fast64_t state = 0; state < mecIndices.size(); ++ state) {
+        if (mecIndices[state] == k) {
+            for (uint_fast64_t l = 0; l <= l_max; ++ l) {
+                for (const auto &keyValue : windowVector[state][l]) {
+                    unfoldedStates[keyValue.second].state = state;
+                    unfoldedStates[keyValue.second].currentSumOfWeights = keyValue.first;
+                    unfoldedStates[keyValue.second].currentWindowLength = l;
+                }
+            }
+        }
+    }
+
+    return unfoldedStates;
+}
+
+
+template class sw::BndGoodWindowMP::ECsUnfolding<double>;
+template class sw::BndGoodWindowMP::ECsUnfolding<storm::RationalNumber>;
--- stochastic-windows/ECsUnfolding.h
+++ stochastic-windows/ECsUnfolding.h
@@ -5,35 +5,46 @@
 #include <iostream>
 #include <storm/models/sparse/Mdp.h>
 #include <storm/storage/SparseMatrix.h>
+#include <storm/storage/MaximalEndComponent.h>
+#include <storm/storage/MaximalEndComponentDecomposition.h>
+#include <storm/models/sparse/Mdp.h>
 
 #ifndef STORM_BNDGOODWINDOWMP_H
 #define STORM_BNDGOODWINDOWMP_H
 
-#endif //STORM_BNDGOODWINDOWMP_H
-
 namespace sw {
     namespace BndGoodWindowMP {
+
+        template<typename ValueType>
+        struct StateWeightWindowLength{
+            uint_fast64_t state;
+            ValueType currentSumOfWeights;
+            uint_fast64_t currentWindowLength;
+        };
+
         template<typename ValueType>
         class ECsUnfolding {
         public:
 
-            /**
-             * Unfold the end components of the mdp entered in parameter for the window mean payoff problem w.r.t. the
-             * maximal windows' length l_max.
+            /*!
+             * Constructs an unfolding of the end components of the mdp entered in parameter for the window mean payoff
+             * problem w.r.t. the maximal windows' length l_max.
              *
              * @param mdp Markov decision process for which the ECs will be unfolded
              * @param rewardModelName name of the reward model following which the mdp will be unfolded
              * @param l_max the maximum length of windows to consider
              */
-            ECsUnfolding(storm::models::sparse::Mdp<ValueType, storm::models::sparse::StandardRewardModel<ValueType>> const& mdp,
+            ECsUnfolding(storm::models::sparse::Mdp<ValueType,storm::models::sparse::StandardRewardModel<ValueType>> const& mdp,
                          std::string const& rewardModelName,
                          uint_fast64_t const& l_max);
-            /**
+
+            /*!
              * Get the index of the MEC containing the input state. Note that 0 is a special value indicating that the
              * input state does not belong to any MEC
              */
             uint_fast64_t getMecIndex(uint_fast64_t state);
-            /**
+
+            /*!
              * Get the index k of the MEC containing the input state as well as the index of the state
              * (state, currentSumOfWeights, currentWindowLength) in the kth matrix, being the matrix representing the
              * unfolding of the kth MEC.
@@ -44,52 +55,87 @@ namespace sw {
              * @param currentSumOfWeights value of the current sum of weights in the window
              * @param currentWindowLength value of the current window length
              */
-            std::pair<uint_fast64_t, uint_fast64_t> getNewIndex(uint_fast64_t state, ValueType currentSumOfWeights,
-                                                                uint_fast64_t currentWindowLength);
-            /**
+            const std::pair<uint_fast64_t, uint_fast64_t> getNewIndex(uint_fast64_t state,
+                                                                      ValueType currentSumOfWeights,
+                                                                      uint_fast64_t currentWindowLength);
+            /*!
              * Returns the matrix representing the unfolding of the kth MEC of the original MDP.
              * Note that 0 is a special value and does not represent any MEC.
              */
-            storm::storage::SparseMatrix<ValueType> getUnfoldedMatrix(uint_fast64_t mec);
+            const storm::storage::SparseMatrix<ValueType> getUnfoldedMatrix(uint_fast64_t mec);
+
+            /*!
+             * Get the number of unfolded ECs.
+             */
+            uint_fast64_t getNumberOfUnfoldedECs();
+
+            /*!
+             * Get the maximum windows length.
+             */
+            uint_fast64_t getMaximumWindowsLength();
+
+            /*!
+             * Get a vector containing, for each MEC k, the meaning of each state in the new matrix corresponding to the
+             * unfolding of the MEC k, expressed as a tuple (s, w, l) where s (state) is the state in the original matrix,
+             * w (currentSumOfWeights) is the current sum of weights in the unfolding and l (currentWindowLength) is
+             * the current window length in the unfolding.
+             *
+             * @param k the index of the MEC containing the state for which the meaning is explained.
+             */
+            const std::vector<StateWeightWindowLength<ValueType>> getNewStatesMeaning(uint_fast64_t k);
 
         private:
 
-            /**
+            /*!
+             * Maximum windows length
+             */
+            uint_fast64_t l_max;
+
+            /*!
              * For each MEC in the MEC decomposition, this matrix represents the unfolded MEC for the window MP
              * objective.
              */
             std::vector<storm::storage::SparseMatrix<ValueType>> matrices;
-            /**
+
+            /*!
              * Vector containing the index of the MEC of each state.
              * Note that 0 is a special value indicating that the state does not belong to any MEC.
              */
             std::vector<uint_fast64_t> mecIndices;
+
+            /*!
+             * This vector contains the index of each original state s in the unfolding regarding to
+             * the current window length l and the current sum of weights w in the unfolding of the MEC containing it.
+             * usage: the index of the state s in the unfolding of the associated MEC is windowVector[s][l][w]
+             */
             std::vector<std::vector<std::unordered_map<ValueType, uint_fast64_t>>> windowVector;
-            /**
+
+            /*!
              * For each MEC of the original MDP, this vector maps each new state to its pairs of
              * (successor states, probabilities) for each of its enabled actions that are ordered according to the
              * ordering of enabled actions of each state of the MEC.
-             * Dimensions = (0: MEC, 1: state, 2: action, 3: pair of (s', p) where p is the probability to go to s').
+             * Dimensions = (0: MEC, 1: state, 2: action, 3: pairs of (s', p) where p is the probability to go to s').
              */
-            std::vector<std::vector<std::vector<std::pair<uint_fast64_t, ValueType>>>> newRowGroupEntries;
-            /**
+            std::vector<std::vector<std::vector<std::vector<std::pair<uint_fast64_t, ValueType>>>>> newRowGroupEntries;
+
+            /*!
              * Unfold a MEC from a given state. The result fields oldToNewStateMapping and newRowGroupEntries are
              * filled accordingly.
              *
              * @param state
              * @param currentSumOfWeights
              * @param l
-             * @param l_max
              * @param originalMatrix
              * @param rewardModel
              * @param currentMec
              * @return the index of the input state in the new matrix of the current MEC
              */
-            uint_fast64_t unfoldFrom(u_int64_t const& state, ValueType const& currentSumOfWeights, uint_fast64_t const& l,
-                                     uint_fast64_t const& l_max,
+            uint_fast64_t unfoldFrom(uint_fast64_t const& state, ValueType const& currentSumOfWeights, uint_fast64_t const& l,
                                      storm::storage::SparseMatrix<ValueType> const &originalMatrix,
                                      std::vector<ValueType> const& stateActionRewardsVector,
                                      storm::storage::MaximalEndComponent const &currentMec);
         };
     }
 }
+
+#endif //STORM_BNDGOODWINDOWMP_H
