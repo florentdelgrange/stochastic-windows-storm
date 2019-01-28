f39ece9166f15b1ca42690645e35f3be294609ea
radioGiorgio <florent.delgrange@gmail.com>
2019-01-28 13:41:21 +0100

    Good win for mean payoff

---
+++ stochastic-windows/game/WindowGame.cpp
@@ -0,0 +1,141 @@
+//
+// Created by Florent Delgrange on 2019-01-25.
+//
+
+#include "WindowGame.h"
+
+namespace sw {
+    namespace Game {
+
+        template<typename ValueType>
+        WindowGame<ValueType>::WindowGame(
+                storm::models::sparse::Mdp <ValueType, storm::models::sparse::StandardRewardModel<ValueType>> const &mdp,
+                std::string const &rewardModelName,
+                uint_fast64_t const &l_max,
+                storm::storage::BitVector const &restrictedStateSpace,
+                storm::storage::BitVector const &enabledActions)
+                : mdp(mdp),
+                  matrix(mdp.getTransitionMatrix()),
+                  rewardModel(mdp.getRewardModel(rewardModelName)),
+                  l_max(l_max),
+                  restrictedStateSpace(restrictedStateSpace),
+                  enabledActions(enabledActions) {
+                      assert(restrictedStateSpace.size() == mdp.getNumberOfStates());
+                      assert(enabledActions.size() == this->matrix.getRowCount());
+                  }
+
+        template<typename ValueType>
+        WindowMeanPayoffGame<ValueType>::WindowMeanPayoffGame(
+                storm::models::sparse::Mdp <ValueType, storm::models::sparse::StandardRewardModel<ValueType>> const &mdp,
+                std::string const &rewardModelName,
+                uint_fast64_t const &l_max,
+                storm::storage::BitVector const &restrictedStateSpace,
+                storm::storage::BitVector const &enabledActions)
+                : WindowGame<ValueType>(mdp, rewardModelName, l_max, restrictedStateSpace, enabledActions) {}
+
+        template<typename ValueType>
+        WindowParityGame<ValueType>::WindowParityGame(
+                storm::models::sparse::Mdp <ValueType, storm::models::sparse::StandardRewardModel<ValueType>> const &mdp,
+                std::string const &rewardModelName,
+                uint_fast64_t const &l_max,
+                storm::storage::BitVector const &restrictedStateSpace,
+                storm::storage::BitVector const &enabledActions)
+                : WindowGame<ValueType>(mdp, rewardModelName, l_max, restrictedStateSpace, enabledActions) {}
+
+        template<typename ValueType>
+        std::vector<storm::storage::BitVector> WindowGame<ValueType>::getSuccessorStates() {
+
+            std::vector<storm::storage::BitVector> successors(this->mdp.getNumberOfChoices(),
+                    storm::storage::BitVector(this->mdp.getNumberOfStates(), false));
+            for (uint_fast64_t action: this->enabledActions) {
+                for (auto const& successorEntry : this->matrix.getRow(action)) {
+                    if (this->restrictedStateSpace[successorEntry.getColumn()]) {
+                        successors[action].set(successorEntry.getColumn(), true);
+                    }
+                }
+            }
+            return successors;
+        }
+
+        template<typename ValueType>
+        storm::storage::BitVector WindowMeanPayoffGame<ValueType>::goodWin() {
+            // C[l][s] is the best sum that can be ensured from state s in at most l steps
+            std::vector<std::vector<ValueType>> C(this->l_max, std::vector<ValueType>(this->restrictedStateSpace.getNumberOfSetBits()));
+            // To avoid C to have a size of l_max X number of states (but rather l_max X number of restricted states)
+            std::vector<uint_fast64_t> oldToNewStateMapping(this->mdp.getNumberOfStates());
+            std::vector<uint_fast64_t> const& stateActionIndices = this->matrix.getRowGroupIndices();
+            std::vector<ValueType> const& weight = this->rewardModel.getStateActionRewardVector();
+            // to deduct the number of enabled actions of each state
+            std::vector<uint_fast64_t> const& numberOfEnabledActionsBeforeIndices = this->enabledActions.getNumberOfSetBitsBeforeIndices();
+            // winning set
+            storm::storage::BitVector winningSet(this->mdp.getNumberOfStates(), false);
+
+            // Initialization of the matrix C
+            uint_fast64_t s = 0;
+            for (uint_fast64_t state: this->restrictedStateSpace) {
+                uint_fast64_t action = this->enabledActions.getNextSetIndex(stateActionIndices[state]);
+                C[0][s] = weight[action];
+                // iterate on enabled actions of state s
+                for (action = this->enabledActions.getNextSetIndex(action + 1);
+                     action < stateActionIndices[state + 1];
+                     action = this->enabledActions.getNextSetIndex(action + 1)) {
+                    if (weight[action] > C[0][s]) {
+                        C[0][s] = weight[action];
+                    }
+                }
+                oldToNewStateMapping[state] = s;
+                ++ s;
+            }
+            // Fill in C up to l_max
+            for (uint_fast64_t i = 1; i < this->l_max; ++ i) {
+                for (uint_fast64_t state: this->restrictedStateSpace) {
+                    s = oldToNewStateMapping[state];
+                    std::vector<ValueType> actionValues;
+                    uint_fast64_t numberOfEnabledActions;
+                    if (state == this->mdp.getNumberOfStates() - 1) {
+                        numberOfEnabledActions = this->enabledActions.getNumberOfSetBits() -
+                                numberOfEnabledActionsBeforeIndices[stateActionIndices[state]];
+                    }
+                    else {
+                        numberOfEnabledActions = numberOfEnabledActionsBeforeIndices[stateActionIndices[state]] -
+                                numberOfEnabledActionsBeforeIndices[stateActionIndices[state + 1]];
+                    }
+                    actionValues.reserve(numberOfEnabledActions);
+                    for (uint_fast64_t action = this->enabledActions.getNextSetIndex(stateActionIndices[state]);
+                         action < stateActionIndices[state + 1];
+                         action = this->enabledActions.getNextSetIndex(action + 1)) {
+                        std::vector<ValueType> successorValues;
+                        // We assume that all entries (successors) of the row corresponding to the current action are in
+                        // the restricted state space
+                        successorValues.reserve(this->matrix.getRow(action).getNumberOfEntries());
+                        for (auto const &successorEntry : this->matrix.getRow(action)) {
+                            uint_fast64_t s_prime = oldToNewStateMapping[successorEntry.getColumn()];
+                            successorValues.push_back(C[i - 1][s_prime]);
+                        }
+                        ValueType worstSuccessorValue = storm::utility::minimum(successorValues);
+                        actionValues.push_back(storm::utility::max<ValueType>(weight[action], weight[action] + worstSuccessorValue));
+                    }
+                    C[i][s] = storm::utility::maximum(actionValues);
+
+                    // Construct the winning set
+                    if (i == this->l_max - 1 && C[i][s] >= 0) {
+                        winningSet.set(state, true);
+                    }
+                }
+            }
+            return winningSet;
+        }
+
+        template<typename ValueType>
+        storm::storage::BitVector WindowParityGame<ValueType>::goodWin() {
+
+        }
+
+        template class WindowGame<double>;
+        template class WindowGame<storm::RationalNumber>;
+        template class WindowMeanPayoffGame<double>;
+        template class WindowMeanPayoffGame<storm::RationalNumber>;
+        template class WindowParityGame<double>;
+
+    }
+}
--- stochastic-windows/game/WindowGame.h
+++ stochastic-windows/game/WindowGame.h
@@ -2,6 +2,10 @@
 // Created by Florent Delgrange on 2019-01-25.
 //
 
+#include <storm/storage/BitVector.h>
+#include <storm/models/sparse/Mdp.h>
+#include <storm/storage/SparseMatrix.h>
+
 #ifndef STORM_WINDOWGAME_H
 #define STORM_WINDOWGAME_H
 
@@ -18,13 +22,20 @@ namespace sw {
              * @param mdp the model to consider as a game
              * @param rewardModelName the name of the reward model to consider
              * @param l_max the maximum window size
+             * @param restrictedStateSpace the set of states to consider in the MDP-game
              * @param enabledActions the set of actions to consider in the MDP-game
              */
             WindowGame(
-                    storm::models::sparse::Mdp<ValueType,storm::models::sparse::StandardRewardModel<ValueType>> const& mdp,
-                    std::string const& rewardModelName,
-                    uint_fast64_t const& l_max,
-                    storm::storage::BitVector const& enabledActions);
+                    storm::models::sparse::Mdp <ValueType, storm::models::sparse::StandardRewardModel<ValueType>> const &mdp,
+                    std::string const &rewardModelName,
+                    uint_fast64_t const &l_max,
+                    storm::storage::BitVector const &restrictedStateSpace,
+                    storm::storage::BitVector const &enabledActions);
+
+            /*
+             * Make destructor virtual to allow deleting objects through pointer to base class(es).
+             */
+            virtual ~WindowGame() {}
 
             /**
              * Compute the winning set of states from which there exists a strategy allowing to surely close a window in
@@ -38,24 +49,38 @@ namespace sw {
             /**
              * MDP to consider as a game
              */
-            storm::models::sparse::Mdp<ValueType,storm::models::sparse::StandardRewardModel<ValueType>> const& mdp,
+            storm::models::sparse::Mdp <ValueType, storm::models::sparse::StandardRewardModel<ValueType>> const &mdp;
+            /**
+             * matrix of the MDP to consider as a game
+             */
+            storm::storage::SparseMatrix<ValueType> const &matrix;
             /**
              * Reward Model of the MDP
              */
-            storm::models::sparse::StandardRewardModel<ValueType> const& rewardModel;
+            storm::models::sparse::StandardRewardModel <ValueType> const &rewardModel;
             /**
              * maximum window size
              */
-            uint_fast64_t const& l_max,
+            uint_fast64_t const &l_max;
             /**
              * set of actions to consider in the MDP-game
              */
-            storm::storage::BitVector const& enabledActions;
+            storm::storage::BitVector const &restrictedStateSpace;
+            /**
+             * set of actions to consider in the MDP-game
+             */
+            storm::storage::BitVector const &enabledActions;
+
+            /**
+             * Compute the set of successor states of each (state, action) pair w.r.t. the restricted state space and
+             * enabled actions considered.
+             */
+            std::vector<storm::storage::BitVector> getSuccessorStates();
 
         };
 
         template<typename ValueType>
-        class WindowGameMeanPayoff: public WindowGame {
+        class WindowMeanPayoffGame : public WindowGame<ValueType> {
         public:
 
             /**
@@ -64,20 +89,22 @@ namespace sw {
              * @param mdp the model to consider as a game
              * @param rewardModelName the name of the reward model to consider
              * @param l_max the maximum window size
+             * @param restrictedStateSpace the set of states to consider in the MDP-game
              * @param enabledActions the set of actions to consider in the MDP-game
              */
-            WindowGameMeanPayoff(
-                    storm::models::sparse::Mdp<ValueType,storm::models::sparse::StandardRewardModel<ValueType>> const& mdp,
-                    std::string const& rewardModelName,
-                    uint_fast64_t const& l_max,
-                    storm::storage::BitVector const& enabledActions);
+            WindowMeanPayoffGame(
+                    storm::models::sparse::Mdp <ValueType, storm::models::sparse::StandardRewardModel<ValueType>> const &mdp,
+                    std::string const &rewardModelName,
+                    uint_fast64_t const &l_max,
+                    storm::storage::BitVector const &restrictedStateSpace,
+                    storm::storage::BitVector const &enabledActions);
 
             storm::storage::BitVector goodWin() override;
 
         };
 
         template<typename ValueType>
-        class WindowGameParity: public WindowGame {
+        class WindowParityGame : public WindowGame<ValueType> {
         public:
 
             /**
@@ -86,17 +113,20 @@ namespace sw {
              * @param mdp the model to consider as a game
              * @param rewardModelName the name of the reward model to consider
              * @param l_max the maximum window size
+             * @param restrictedStateSpace the set of states to consider in the MDP-game
              * @param enabledActions the set of actions to consider in the MDP-game
              */
-            WindowGameMeanParity(
-                    storm::models::sparse::Mdp<ValueType,storm::models::sparse::StandardRewardModel<ValueType>> const& mdp,
-                    std::string const& rewardModelName,
-                    uint_fast64_t const& l_max,
-                    storm::storage::BitVector const& enabledActions);
+            WindowParityGame(
+                    storm::models::sparse::Mdp <ValueType, storm::models::sparse::StandardRewardModel<ValueType>> const &mdp,
+                    std::string const &rewardModelName,
+                    uint_fast64_t const &l_max,
+                    storm::storage::BitVector const &restrictedStateSpace,
+                    storm::storage::BitVector const &enabledActions);
 
             storm::storage::BitVector goodWin() override;
 
         };
+    }
 }
 
 
