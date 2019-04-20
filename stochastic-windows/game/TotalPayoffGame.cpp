611149b64f84fb4685a42d3e4865d8ee667d93c8
radioGiorgio <florent.delgrange@gmail.com>
2019-04-20 20:22:14 +0200

    update wrt the new version of SCCs decomposition

--- stochastic-windows/game/TotalPayoffGame.cpp
+++ stochastic-windows/game/TotalPayoffGame.cpp
@@ -318,8 +318,14 @@ namespace sw {
             clock_t start = clock();
              */
 
+            storm::storage::StronglyConnectedComponentDecompositionOptions sccOptions;
+            sccOptions.dropNaiveSccs(false); sccOptions.onlyBottomSccs(false); sccOptions.computeSccDepths(false);
+            storm::storage::BitVector subSystem = this->restrictedStateSpace;
+            storm::storage::BitVector choices = this->enabledActions;
+            // sccOptions.subsystem(&subSystem); sccOptions.choices(&choices);
+            sccOptions.subsystem(&this->restrictedStateSpace); sccOptions.choices(&this->enabledActions);
             storm::storage::StronglyConnectedComponentDecomposition<ValueType>
-            stronglyConnectedComponentDecomposition(this->matrix, this->restrictedStateSpace, this->enabledActions);
+            stronglyConnectedComponentDecomposition(this->matrix, sccOptions);
 
             for (const auto &component: stronglyConnectedComponentDecomposition) {
                 storm::storage::BitVector sccStates(this->restrictedStateSpace.size(), false);
