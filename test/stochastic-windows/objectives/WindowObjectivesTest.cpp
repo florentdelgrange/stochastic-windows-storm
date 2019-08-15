dfcfd7c8186698496401c73d11fbb94eccde32dc
radioGiorgio <florent.delgrange@gmail.com>
2019-08-15 16:30:31 +0200

    MEC classification

--- test/stochastic-windows/objectives/WindowObjectivesTest.cpp
+++ test/stochastic-windows/objectives/WindowObjectivesTest.cpp
@@ -3,6 +3,8 @@
 //
 
 #include <storm/settings/modules/MinMaxEquationSolverSettings.h>
+#include <stochastic-windows/fixedwindow/MaximalEndComponentClassifier.h>
+#include <stochastic-windows/prefixindependent/MaximalEndComponentDecompositionUnfolding.h>
 #include "gtest/gtest.h"
 #include "storm-parsers/parser/PrismParser.h"
 #include "storm/builder/ExplicitModelBuilder.h"
@@ -11,10 +13,15 @@
 
 namespace  {
 
+
     class ValueIterationSolverInitialization {
     public:
         ValueIterationSolverInitialization() {
-            storm::settings::mutableManager().setFromString("--minmax:method vi");
+            auto const& minMaxSettings = storm::settings::getModule<storm::settings::modules::MinMaxEquationSolverSettings>();
+            auto minMaxEquationSolvingTechnique = minMaxSettings.getMinMaxEquationSolvingMethod();
+            if (minMaxEquationSolvingTechnique != storm::solver::MinMaxMethod::ValueIteration) {
+                storm::settings::mutableManager().setFromString("--minmax:method vi");
+            }
         }
     };
 
@@ -80,6 +87,34 @@ namespace  {
         EXPECT_NEAR(0, result.values[9], this->precision);
         EXPECT_NEAR(0.3125, result.values[10], this->precision);
         EXPECT_NEAR(1, result.values[11], this->precision);
+    }
+
+    TYPED_TEST(WindowObjectiveTest, mecClassificationTest) {
+        // Unfoldings
+        // Parity
+        sw::storage::MaximalEndComponentDecompositionUnfoldingParity<double> unfoldingPar(*this->getMDP(), "priorities", 3);
+        // Mean Payoff
+        sw::storage::MaximalEndComponentDecompositionUnfoldingMeanPayoff<double> unfoldingMp(*this->getMDP(), "weights", 3);
+
+        // expected sets
+        storm::storage::BitVector safeStates(12, false);
+        storm::storage::BitVector goodStates(12, false);
+        safeStates.set(3); safeStates.set(4); safeStates.set(6); safeStates.set(7); safeStates.set(11);
+        goodStates.set(1); goodStates.set(3); goodStates.set(4); goodStates.set(6); goodStates.set(7); goodStates.set(11);
+
+        // Classification by unfolding MECs (Par)
+        sw::FixedWindow::MaximalEndComponentClassifier<double> classifierUnfoldingPar(*this->getMDP(), unfoldingPar);
+        ASSERT_EQ(safeStates, classifierUnfoldingPar.getSafeStateSpace());
+        ASSERT_EQ(goodStates, classifierUnfoldingPar.getGoodStateSpace());
+        // Classification by unfolding MECs (MP)
+        sw::FixedWindow::MaximalEndComponentClassifier<double> classifierUnfoldingMP(*this->getMDP(), unfoldingMp);
+        ASSERT_EQ(safeStates, classifierUnfoldingMP.getSafeStateSpace());
+        ASSERT_EQ(goodStates, classifierUnfoldingMP.getGoodStateSpace());
+        // Classification by considering MECs as games (MP)
+        sw::storage::MaximalEndComponentDecompositionWindowMeanPayoffGame<double> games(*this->getMDP(), "weights", 3);
+        sw::FixedWindow::MaximalEndComponentClassifier<double> gameClassifier(*this->getMDP(), games);
+        ASSERT_EQ(classifierUnfoldingMP.getSafeStateSpace(), gameClassifier.getSafeStateSpace());
+        ASSERT_EQ(classifierUnfoldingMP.getGoodStateSpace(), gameClassifier.getGoodStateSpace());
     }
 
 }
